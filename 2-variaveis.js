e2c8055b5a660ad059ed5bda712f0bfff42d16d7
Bergomezz <bernardogomezz@yahoo.com.br>
2021-08-09 16:00:48 -0300

    Operações Aritiméticas

---
+++ 2-variaveis.js
@@ -0,0 +1,10 @@
+//JS é case sensitive.
+console.log('Trabalhando com Variáveis');
+
+const idade = 36;
+const nome = 'Bernardo'
+let ano = 2021;
+
+console.log('Seu nome é:', nome);
+console.log('Sua idade é:', idade);
+console.log('O ano que você está é:', ano);
