99572e666a9e6ce9eadd150b8c5ee9be872b481c
Bergomezz <bernardogomezz@yahoo.com.br>
2021-08-09 13:28:29 -0300

    começandoaula2

---
+++ app.js
@@ -0,0 +1,9 @@
+//JS é case sensitive.
+console.log('Trabalhando com Variáveis');
+
+const idade = 36;
+const nome = 'Bernardo'
+
+console.log('Seu nome é:', nome);
+console.log('Sua idade é:', idade);
+
