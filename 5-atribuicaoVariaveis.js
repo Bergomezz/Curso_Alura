1d48aa2240784f32d765c6b0ec592befb4d5c8c6
Bergomezz <bernardogomezz@yahoo.com.br>
2021-08-10 14:11:36 -0300

    Atualizando a lista

---
+++ 5-atribuicaoVariaveis.js
@@ -0,0 +1,22 @@
+console.log('Trabalhando com Atribuição de Variáveis');
+
+let primeiroNome = 'Bernardo'
+const sobrenome = 'Gomes'
+let ano = 2021;
+
+console.log('Seu nome é:', primeiroNome, sobrenome);
+console.log('Sua idade é:', idade);
+console.log('O ano que você está é:', ano);
+
+console.log(`Seu nome é: ${primeiroNome} ${sobrenome}
+Sua idade é: ${idade}
+O ano que você está é: ${ano}
+`);
+
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
+
+
+let idade;
+idade=36;
+idade = idade+1;
---
+++ app.js
@@ -0,0 +1,16 @@
+// escrever códigos para passar aos outros novos arquivos
+
+// const salvador = `Salvador`;
+// const saoPaulo = `São Paulo`;
+// const rioDeJaneiro = `Rio de Janeiro`;
+
+const listaDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+);
+
+listaDestinos.push(`Curitiba`); //Adiciona um item na lista.
+console.log('Destinos Possives');
+// console.log(salvador, saoPaulo, rioDeJaneiro)
+console.log(listaDestinos);
