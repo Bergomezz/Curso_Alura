faee0e52838c8d112b75480faac57d7789630ee7
Bergomezz <bernardogomezz@yahoo.com.br>
2021-08-10 12:48:33 -0300

    atribuição de variaveis2

--- app.js
+++ app.js
@@ -2,15 +2,18 @@
 console.log('Trabalhando com Atribuição de Variáveis');
 
 const idade = 36;
-const nome = 'Bernardo'
+let nome = 'Bernardo'
+const sobrenome = 'Gomes'
 let ano = 2021;
 
-console.log('Seu nome é:', nome);
+console.log('Seu nome é:', nome, sobrenome);
 console.log('Sua idade é:', idade);
 console.log('O ano que você está é:', ano);
 
-console.log(`Seu nome é: ${nome}
+console.log(`Seu nome é: ${nome} ${sobrenome}
 Sua idade é: ${idade}
 O ano que você está é: ${ano}
 `);
 
+const nomeCompleto = nome + sobrenome;
+console.log(nomeCompleto);
