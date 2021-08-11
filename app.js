a7a4ca26c9cd82e77177db5ea2c49251546cb732
Bergomezz <bernardogomezz@yahoo.com.br>
2021-08-11 14:58:40 -0300

    Inclusão do modulo teclado

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-// escrever códigos para passar aos outros novos arquivos
+// Tentando Pensar em algum exercicios pra fazer.
+
+const readline = require('readline-sync');
+const numeroPedido = readline.question('Número do Pedido:');
+const enderecoDeEntrega = readline.question('Seu Endereço:');
+const horaDoPedido = readline.question('O Pedido foi Feito as:');
+
+let rota = {
+    numeroPedido, 
+    enderecoDeEntrega,
+    horaDoPedido
+};
+
+const numeroPedido1 = readline.question('Número do Pedido:');
+const enderecoDeEntrega1 = readline.question('Seu Endereço:');
+const horaDoPedido1 = readline.question('O Pedido foi Feito as:');
+
+let rota1 = {
+    numeroPedido1, 
+    enderecoDeEntrega1,
+    horaDoPedido1
+};
+
+if(enderecoDeEntrega==enderecoDeEntrega1){
+    let bloco = (rota + rota1);
+    console.log(bloco);
+}else{
+    console.log(rota);
+    console.log(rota1);
+};
