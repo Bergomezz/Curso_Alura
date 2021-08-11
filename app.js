91648a9c081bae39723e6b6c08c72a5011d371c6
Bergomezz <bernardogomezz@yahoo.com.br>
2021-08-10 21:16:38 -0300

    Termino aula if else

--- app.js
+++ app.js
@@ -9,19 +9,41 @@ const listaDestinos = new Array(
 
 const idadeComprador = 15;
 const estaAcompanhada = true;
+const temPassagemComprada = true
+
+// console.log('Destinos Possives');
+// console.log(listaDestinos);
+
+// if (idadeComprador >= 18) {
+//     console.log('Comprador maior de idade');
+//     listaDestinos.splice(1, 1);
+// } else if (estaAcompanhada) {
+//     console.log('Comprador está acompanhado');
+//     listaDestinos.splice(1, 1);
+// } else {
+//     console.log('Comprador não é maior de idade!');
+// };
+
+
+// console.log(listaDestinos);
+
 
 console.log('Destinos Possives');
 console.log(listaDestinos);
 
-if (idadeComprador >= 18) {
-    console.log('Comprador maior de idade');
-    listaDestinos.splice(1, 1);
-} else if (estaAcompanhada) {
-    console.log('Comprador está acompanhado');
-    listaDestinos.splice(1, 1);
-} else {
+if (idadeComprador >= 18 || estaAcompanhada ) {
+    console.log('Boa viagem');
+    listaDestinos.splice(2, 1);
+}else {
     console.log('Comprador não é maior de idade!');
 };
 
 
 console.log(listaDestinos);
+
+console.log('Embarque: \n\n');
+if(idadeComprador >= 18 ||estaAcompanhada && temPassagemComprada){
+    console.log('Boa viagem!');
+}else{
+    console.log('Embarque Negado!')
+};
