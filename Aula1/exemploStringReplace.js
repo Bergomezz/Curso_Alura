aacad81466d61f84db5c61d4bea1665b4d627795
Bergomezz <bernardogomezz@yahoo.com.br>
2021-08-11 16:56:48 -0300

    StrigReplace

--- Aula1/exemploStringReplace.js
+++ Aula1/exemploStringReplace.js
@@ -1,4 +1,4 @@
-const cpfSemFormatacao = 'CPF 00156145308';
-const cpfFormatado = cpfSemFormatacao.replace('00156145308', '001.561.453-08');
+const cpfSemFormatacao = '00156145308';
+const cpfFormatado = cpfSemFormatacao.replace(/(\d{3})?(\d{3})?(\d{3})?(\d{2})/, "$1.$2.$3-$4");
 
 console.log(cpfFormatado);
