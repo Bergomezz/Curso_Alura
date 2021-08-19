7ab489013b5fa09cbbc7591b574c534ce06a22c1
Bergomezz <bernardogomezz@yahoo.com.br>
2021-08-18 21:12:10 -0300

    Aula4

--- Aula2/index.js
+++ Aula2/index.js
@@ -9,13 +9,13 @@ const gerente = new Gerente('Bernardo', 5000,98765432101);
 gerente.cadastrarSenha('123');
 const cliente = new Cliente('Lais', 99988877701, '456')
 
-let estaLogado = [];
+// let estaLogado = [];
 
-estaLogado.push = () => {
-    const diretorEstaLogado = SistemaAutenticacao.login(diretor, '123456');
-    const gerenteEstaLogado = SistemaAutenticacao.login(gerente, '123');
-    const clienteEstaLogado = SistemaAutenticacao.login(cliente, '456');
-};
+// estaLogado.push = () => {
+// };
+const diretorLogado = SistemaAutenticacao.login(diretor, '123456');
+const gerenteLogado = SistemaAutenticacao.login(gerente, '123');
+const clienteLogado = SistemaAutenticacao.login(cliente, '456');
 
 // console.log(clienteEstaLogado);
 console.log(estaLogado);
