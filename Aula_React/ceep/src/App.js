cc680726d1835ebdb132a720de99cade5ae501bc
Bergomezz <bernardogomezz@yahoo.com.br>
2021-09-21 12:55:19 -0300

    Ciclo Funções Rect

--- Aula_React/ceep/src/App.js
+++ Aula_React/ceep/src/App.js
@@ -4,30 +4,45 @@ import FormularioCadastro from "./components/FormularioCadastro";
 import "./assets/App.css";
 import './assets/index.css';
 class App extends Component {
-  constructor(){
-    super();    
+
+  constructor() {
+    super();
+
     this.state = {
       notas: []
     }
   }
 
-  criarNota(titulo, texto){
-    const novaNota = {titulo, texto};
+  criarNota(titulo, texto) {
+    const novaNota = { titulo, texto };
     const novoArrayNotas = [...this.state.notas, novaNota]
     const novoEstado = {
       notas: novoArrayNotas
     }
-    this.setState (novoEstado)
+    this.setState(novoEstado)
   }
+
+  deletarNota(index) {
+    let arrayNotas = this.state.notas;
+    arrayNotas.splice(index, 1);
+    this.setState({ notas: arrayNotas })
+  }
+
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
-        <ListaDeNotas notas = {this.state.notas}/>
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <main>
+          <ListaDeCategorias />
+          <ListaDeNotas
+            apagarNota={this.deletarNota.bind(this)}
+            notas={this.state.notas} />
+        </main>
+
       </section>
     );
   }
 }
 
-
+//new ListaDeNotas({notas:this.notas})
 export default App;
