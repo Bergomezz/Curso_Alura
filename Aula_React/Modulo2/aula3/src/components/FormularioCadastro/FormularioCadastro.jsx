00218f951b51377f9b718a4a1d6b308a2d189a7b
Bergomezz <bernardogomezz@yahoo.com.br>
2021-09-30 17:01:25 -0300

    Aula 3 Modulo 2

---
+++ Aula_React/Modulo2/aula3/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function FormularioCadastro() {
+    return (
+        <form>
+            <label>Nome</label>
+            <input type='text' />
+
+            <label>Sobrenome</label>
+            <input type='text' />
+
+            <label>CPF</label>
+            <input type='text' />
+        </form>
+    );
+
+
+
+}
+
+export default FormularioCadastro;
