bae6f973a9da36f19d496dd2f2ce80e13e46655b
Bergomezz <bernardogomezz@yahoo.com.br>
2021-09-30 15:41:01 -0300

    finalização aula react

--- Aula_React/ceep/src/components/ListaDeCategorias/listaDeCategorias.jsx
+++ Aula_React/ceep/src/components/ListaDeCategorias/listaDeCategorias.jsx
@@ -2,21 +2,26 @@ import React, { Component } from "react";
 import "./estilo.css"
 
 class ListaDeCategorias extends Component {
-    constructor(){
+    constructor() {
         super();
-        this.state = {categorias:[]};
+        this.state = { categorias: [] };
+        this._novasCategorias = this._novasCategorias.bind(this)
     }
-    componentDidMount(){
-        this.props.categorias.inscrever(this._novasCategorias.bind(this));
+    componentDidMount() {
+        this.props.categorias.inscrever(this._novasCategorias);
     }
 
-    _novasCategorias(categorias){
-        this.setState({...this.state, categorias})
+    componentWillUnmount() {
+        this.props.categoria.desinscrever(this._novasCategorias)
+    }
+
+    _novasCategorias(categorias) {
+        this.setState({ ...this.state, categorias })
     }
 
     _handleEventoInput(e) {
 
-        if (e.key == 'Enter') {
+        if (e.key === 'Enter') {
             let valorCategoria = e.target.value;
             this.props.adicionarCategoria(valorCategoria);
         }
@@ -26,8 +31,8 @@ class ListaDeCategorias extends Component {
             <section className="lista-categorias">
                 <ul className="lista-categorias_lista">
                     {this.state.categorias.map((categoria, index) => {
-                        return <li key={index} 
-                        className="lista-categorias_item">{categoria}</li>
+                        return <li key={index}
+                            className="lista-categorias_item">{categoria}</li>
                     })}
                 </ul>
                 <input type="text"
