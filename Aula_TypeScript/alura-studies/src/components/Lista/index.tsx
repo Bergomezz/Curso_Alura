a5344711d892c75376f70f78994dee63b17685cc
Bergomezz <bernardogomezz@yahoo.com.br>
2021-10-19 15:17:17 -0300

    inclusão dos modulos css e mais algumas linhas no function coponents

--- Aula_TypeScript/alura-studies/src/components/Lista/index.tsx
+++ Aula_TypeScript/alura-studies/src/components/Lista/index.tsx
@@ -1,26 +1,28 @@
 import React from "react";
+import Item from "./Item";
+import style from  './Lista.module.scss'
 
 function Lista() {
+    const tarefas = [{
+        tarefa: 'React',
+        tempo: '02:00:00'
+    },{
+        tarefa: 'JavaScript',
+        tempo: '01:30:00'
+    }, {
+        tarefa: 'TypeScript',
+        tempo: '03:00:00'
+    }]
     return (
-        <aside>
+        <aside className={style.listaTarefas}>
             <h2>Estudos do dia</h2>
             <ul>
-                <li>
-                    <h3>
-                        React
-                    </h3>
-                    <span>
-                        02:00:00
-                    </span>
-                </li>
-                <li>
-                    <h3>
-                        JavaScript
-                    </h3>
-                    <span>
-                        01:00:00
-                    </span>
-                </li>
+                {tarefas.map((item, index) => (
+                    <Item
+                        key={index} 
+                        {...item}
+                    />
+                ))}
             </ul>
         </aside>
     )
