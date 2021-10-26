c40ed585d06167fd94ddb0deb550d3626255a10a
Bergomezz <bernardogomezz@yahoo.com.br>
2021-10-26 14:09:29 -0300

    Refatorando e concluindo o alura-studies

--- Aula_TypeScript/alura-studies/src/components/Lista/Item/index.tsx
+++ Aula_TypeScript/alura-studies/src/components/Lista/Item/index.tsx
@@ -19,7 +19,7 @@ export default function Item({
         <li className={`${style.item} 
             ${selecionado ? style.itemSelecionado : ''} 
             ${completado ? style.itemCompletado : ''}`} 
-            onClick={() => selecionaTarefa({
+            onClick={() => !completado && selecionaTarefa({
             tarefa,
             tempo,
             selecionado,
@@ -32,6 +32,7 @@ export default function Item({
             <span>
                 {tempo}
             </span>
+            {completado && <span className={style.concluido} aria-Label="tarefa completada"></span>}
         </li>
     )
 }
