ae16f52b2f8b9d7c9e7145aa6b0d699775520c2f
Bergomezz <bernardogomezz@yahoo.com.br>
2022-10-14 11:46:45 -0300

    adding code in app.js

--- Aula_JavaScript/LogicaPongJs/app.js
+++ Aula_JavaScript/LogicaPongJs/app.js
@@ -1,2 +1,19 @@
-let app = new PIXI.Application({ width: 1920, height: 1080 })
+let SCREENWIDTH = 1270
+let SCREENHIEGHT = 720
+
+let app = new PIXI.Application({ width: SCREENWIDTH, height: SCREENHIEGHT })
 document.body.appendChild(app.view)
+
+let sprite = PIXI.Sprite.from('/html/img/sample.png')
+
+app.stage.addChild(sprite)
+
+let elapsed = 0.0
+function speed() {
+  sprite.x = 150.0 + Math.cos(elapsed / 50.0) * 100.0
+}
+
+app.ticker.add(delta => {
+  elapsed += delta
+  speed()
+})
