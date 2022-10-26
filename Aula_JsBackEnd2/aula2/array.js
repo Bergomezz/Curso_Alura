8403992dadf085091d1547816d6003943439c003
Bergomezz <bernardogomezz@yahoo.com.br>
2022-10-26 16:42:55 -0300

    second class

---
+++ Aula_JsBackEnd2/aula2/array.js
@@ -0,0 +1,54 @@
+const listaLivros = [
+  {
+    titulo: "Go",
+    preco: 45,
+    editora: 'Folha'
+  },
+  {
+    titulo: "C++",
+    preco: 35,
+    editora: 'Galho'
+  },
+  {
+    titulo: "Java",
+    preco: 30,
+    editora: 'Folha'
+  },
+  {
+    titulo: "PHP",
+    preco: 15,
+    editora: 'Folha'
+  },
+  {
+    titulo: "Elixir",
+    preco: 50,
+    editora: 'Folha'
+  },
+  {
+    titulo: "Rust",
+    preco: 22,
+    editora: 'Galho'
+  },
+  {
+    titulo: "Ruby",
+    preco: 28,
+    editora: 'Galho'
+  },
+  {
+    titulo: "JavaScript",
+    preco: 25,
+    editora: 'Folha'
+  },
+  {
+    titulo: "C#",
+    preco: 33,
+    editora: 'Galho'
+  },
+  {
+    titulo: "Python",
+    preco: 20,
+    editora: 'Galho'
+  },
+]
+
+module.exports = listaLivros
