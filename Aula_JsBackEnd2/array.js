2d56956a0aa90eabba93a2b9aae868f1bdcb16b3
Bergomezz <bernardogomezz@yahoo.com.br>
2022-10-26 13:39:58 -0300

    starting the sort algorithm

---
+++ Aula_JsBackEnd2/array.js
@@ -0,0 +1,62 @@
+const edFolha = [
+  {
+    titulo: "PHP",
+    preco: 15,
+    editora: 'Folha'
+  }, 
+  {
+    titulo: "JavaScript",
+    preco: 25,
+    editora: 'Folha'
+  },
+  {
+    titulo: "Java",
+    preco: 30,
+    editora: 'Folha'
+  },
+  {
+    titulo: "Go",
+    preco: 45,
+    editora: 'Folha'
+  },
+  {
+    titulo: "Elixir",
+    preco: 50,
+    editora: 'Folha'
+  }
+]
+
+const edGalho = [
+  {
+    titulo: "Python",
+    preco: 20,
+    editora: 'Galho'
+  },
+  {
+    titulo: "Rust",
+    preco: 22,
+    editora: 'Galho'
+  },
+  {
+    titulo: "Ruby",
+    preco: 28,
+    editora: 'Galho'
+  },
+  {
+    titulo: "C#",
+    preco: 33,
+    editora: 'Galho'
+  },
+  {
+    titulo: "C++",
+    preco: 35,
+    editora: 'Galho'
+  },
+  {
+    titulo: "Scala",
+    preco: 40,
+    editora: 'Galho'
+  }
+]
+
+module.exports = {edFolha, edGalho}
