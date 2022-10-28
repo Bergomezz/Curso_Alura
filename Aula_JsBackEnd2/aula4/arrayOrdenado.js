492815bd8d0bf56a294e3fa85fefbeeda0897e5f
Bergomezz <bernardogomezz@yahoo.com.br>
2022-10-28 16:54:18 -0300

    binari sort

---
+++ Aula_JsBackEnd2/aula4/arrayOrdenado.js
@@ -0,0 +1,59 @@
+const listaLivros = [
+  {
+    titulo: 'PHP',
+    preco: 15,
+    editora: 'Folha',
+  },
+  {
+    titulo: 'Python',
+    preco: 20,
+    editora: 'Galho',
+  },
+  {
+    titulo: 'Rust',
+    preco: 22,
+    editora: 'Galho',
+  },
+  {
+    titulo: 'JavaScript',
+    preco: 25,
+    editora: 'Folha',
+  },
+  {
+    titulo: 'Ruby',
+    preco: 28,
+    editora: 'Galho',
+  },
+  {
+    titulo: 'Java',
+    preco: 30,
+    editora: 'Folha',
+  },
+  {
+    titulo: 'C#',
+    preco: 33,
+    editora: 'Galho',
+  },
+  {
+    titulo: 'C++',
+    preco: 35,
+    editora: 'Galho',
+  },
+  {
+    titulo: 'Scala',
+    preco: 40,
+    editora: 'Galho',
+  },
+  {
+    titulo: 'Go',
+    preco: 45,
+    editora: 'Folha',
+  },
+  {
+    titulo: 'Elixir',
+    preco: 50,
+    editora: 'Folha',
+  },
+]
+
+module.exports = listaLivros
